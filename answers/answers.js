935a1ad6a4506228b19421e32680918612d5ffc6
Anas Siddiqui <anassiddiqui@Anass-MacBook-Air.local>
2023-02-26 18:42:09 +0530

    Initial Commit

---
+++ answers/answers.js
@@ -0,0 +1,10 @@
+result1 = a.filter(function (v, i) {
+  return !(i % 2);
+});
+
+result2 = a.map(item => item * item);
+
+a = a.filter(function (v, i) {
+  return !(i % 2);
+});
+result3 = a.map(item => item * item);
